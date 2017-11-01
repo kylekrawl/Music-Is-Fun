d63daa1e1ccb89f2f6f197ae2a6e2872fdae9b9d
Kyle Krawl <kyle.krawl@gmail.com>
2017-11-01 16:14:17 -0600

    Initial commit. Built basic layout, styles, and functionality.

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,18 +1,84 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
-  function draw(){
-  }
 
+  function draw(songList) {
+    console.log(songList)
+    var template = ''
+    var genreStyles = itunesService.getGenreStyleClasses()
+    var mostCommonGenreStyle = 'default'
 
+    for (var i = 0; i < songList.length; i++) {
+      var song = songList[i]
+      for (var styleType in genreStyles) {
+        if (genreStyles[styleType].genres.includes(song.genre)) {
+          genreStyles[styleType].count += 1
+        } 
+        if (genreStyles[styleType].count > genreStyles[mostCommonGenreStyle].count) {
+          mostCommonGenreStyle = styleType
+        }
+      }
+    }
 
+    for (var i = 0; i < songList.length; i++) {
+      
+      console.log(mostCommonGenreStyle)
 
+      var albumArt = song.albumArt.replace('100x100bb', '500x500bb') //Increase image resolution
+      var templates = {
+        'music-video': ` <div class="col-sm-12 col-md-6 flex v-center h-center">
+                              <div id="song-wrapper" class="song-wrapper ${genreStyles[mostCommonGenreStyle].elements.songWrapper.styleClass} panel panel-default text-center">
+                                <div class="panel-body">
+                                  <p class="song-title">${song.title}</p>
+                                  <p class="artist-name">${song.artist}</p>
+                                  <video src="${song.preview}" controls></video>
+                                  <p class="album-title">${song.collection}</p>               
+                                  <p class="album-price">Album Price: 
+                                  ${(song.currency === 'USD') ? ('$' + song.albumPrice) : (song.albumPrice + ' ' + song.currency)}</p>
+                                </div>
+                              </div>
+                            </div>
+                  `,
+        'song': ` <div class="col-sm-12 col-md-6 flex v-center h-center">
+                              <div id="song-wrapper" class="song-wrapper ${genreStyles[mostCommonGenreStyle].elements.songWrapper.styleClass} panel panel-default text-center">
+                                <div class="panel-body">
+                                  <img class="song-image" src="${albumArt}" alt="">
+                                  <p class="song-title">${song.title}</p>
+                                  <p class="artist-name">${song.artist}</p>
+                                  <p class="album-title">${song.collection}</p>
+                                  <audio src="${song.preview}" controls></audio>                    
+                                  <p class="album-price">Album Price: 
+                                  ${(song.currency === 'USD') ? ('$' + song.albumPrice) : (song.albumPrice + ' ' + song.currency)}</p>
+                                </div>
+                              </div>
+                            </div>
+                  `
+      }
+      template += templates[song.fileType]
   
+      document.getElementById('songs').innerHTML = template
+    }
+
+    for (var styleType in genreStyles) {
+      if (styleType === mostCommonGenreStyle) {
+        for(var element in genreStyles[styleType].elements) {
+          console.log(genreStyles[styleType].elements[element].id)
+          console.log(genreStyles[styleType].elements[element].styleClass)
+          document.getElementById(genreStyles[styleType].elements[element].id).classList.add(genreStyles[styleType].elements[element].styleClass)
+        }
+      } else {
+        for(var element in genreStyles[styleType].elements) {
+          document.getElementById(genreStyles[styleType].elements[element].id).classList.remove(genreStyles[styleType].elements[element].styleClass)
+        }
+      }
+    }
+
+  }
 }
--- itunes-service.js
+++ itunes-service.js
@@ -1,30 +1,95 @@
-function ItunesService(){
+function ItunesService() {
 
-    this.getMusicByArtist = function(artist) {
-
-      //allows requests to localhost: 8080 otherwise blocked by itunes
-      var url = '//bcw-getter.herokuapp.com/?url=';
-      var url2 = 'https://itunes.apple.com/search?term=' + artist;
-      var apiUrl = url + encodeURIComponent(url2);
-      
-      //changes the button to loading while songs load
-      $('#get-music-button').text('LOADING....');
-      
-      //modifies the objects to reduce the excess data
-      return $.getJSON(apiUrl).then(function(response){
-        var songList = response.results.map(function (song) {
-                  return {
-                      title: song.trackName,
-                      albumArt: song.artworkUrl60,
-                      artist: song.artistName,
-                      collection: song.collectionName,
-                      price: song.collectionPrice,
-                      preview: song.previewUrl
-                    };
-                })
-        //changes button back to GET MUSIC once songs are loaded
-        $('#get-music-button').text('GET MUSIC');
-        return songList;
-      })
+  var genreStyleClasses = {
+    default: {
+      genres: ['default'],
+      count: 0,
+      elements: {
+        headerRow: {
+          id: 'header-row',
+          styleClass: 'header-row-background-default'
+        },
+        songsRow: {
+          id: 'songs-row',
+          styleClass: 'songs-row-background-default'
+        },
+        songWrapper: {
+          id: 'song-wrapper',
+          styleClass: 'song-wrapper-background-default'
+        }
+      }
+    },
+    alternate1: {
+      genres: ['Hip-Hop/Rap'],
+      count: 0,
+      elements: {
+        headerRow: {
+          id: 'header-row',
+          styleClass: 'header-row-background-alternate-1'
+        },
+        songsRow: {
+          id: 'songs-row',
+          styleClass: 'songs-row-background-alternate-1'
+        },
+        songWrapper: {
+          id: 'song-wrapper',
+          styleClass: 'song-wrapper-background-alternate-1'
+        }
+      }
+    },
+    alternate2: {
+      genres: ['Pop', 'Alternative', 'Electronic'],
+      count: 0,
+      elements: {
+        headerRow: {
+          id: 'header-row',
+          styleClass: 'header-row-background-alternate-2'
+        },
+        songsRow: {
+          id: 'songs-row',
+          styleClass: 'songs-row-background-alternate-2'
+        },
+        songWrapper: {
+          id: 'song-wrapper',
+          styleClass: 'song-wrapper-background-alternate-2'
+        }
+      }
     }
+  }
+
+  this.getGenreStyleClasses = function getGenreStyleClasses() {
+    return JSON.parse(JSON.stringify(genreStyleClasses))
+  }
+
+  this.getMusicByArtist = function (artist) {
+
+    //allows requests to localhost: 8080 otherwise blocked by itunes
+    var url = '//bcw-getter.herokuapp.com/?url=';
+    var url2 = 'https://itunes.apple.com/search?term=' + artist;
+    var apiUrl = url + encodeURIComponent(url2);
+
+    //changes the button to loading while songs load
+    $('#get-music-button').text('LOADING....');
+
+    //modifies the objects to reduce the excess data
+    return $.getJSON(apiUrl).then(function (response) {
+      var songList = response.results.map(function (song) {
+        return {
+          title: song.trackName,
+          albumArt: song.artworkUrl100, // Slightly better resolution than using .artworkUrl60
+          artist: song.artistName,
+          collection: song.collectionName,
+          albumPrice: song.collectionPrice,
+          trackPrice: song.trackPrice,
+          preview: song.previewUrl,
+          currency: song.currency,
+          fileType: song.kind,
+          genre: song.primaryGenreName
+        };
+      })
+      //changes button back to GET MUSIC once songs are loaded
+      $('#get-music-button').text('GET MUSIC');
+      return songList;
+    })
+  }
 }
